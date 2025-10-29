366733a13137bfd13388c2210a004c725aa4b2b9
Magno Belloni <bellonidesouza6@gmail.com>
2025-10-29 01:14:52 -0300

    Adicionado Swagger

--- src/BugStore.Api/Program.cs
+++ src/BugStore.Api/Program.cs
@@ -4,15 +4,24 @@ using BugStore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddApplicationDependencies();
 builder.Services.AddInfrastructureDependencies();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 app.MapCustomersEndpoints();
 app.MapProductsEndpoints();
 app.MapOrdersEndpoints();
 app.MapReportsEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BugStore API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.Run();
